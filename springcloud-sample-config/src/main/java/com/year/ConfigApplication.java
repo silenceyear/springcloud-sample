888d999ad4050833eaf6cef22af50205a45c314b
yeqi <597019425@qq.com>
2018-12-26 16:41:19 +0800

    config

--- springcloud-sample-config/src/main/java/com/year/ConfigApplication.java
+++ springcloud-sample-config/src/main/java/com/year/ConfigApplication.java
@@ -2,10 +2,11 @@ package com.year;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableConfigServer
 public class ConfigApplication {
 
