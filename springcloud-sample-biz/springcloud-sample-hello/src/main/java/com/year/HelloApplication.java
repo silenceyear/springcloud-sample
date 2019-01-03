4a426be0684faa7cd903056f262f664bf7f3c629
yeqi <597019425@qq.com>
2019-01-03 13:22:44 +0800

    sample biz

--- springcloud-sample-biz/springcloud-sample-hello/src/main/java/com/year/HelloApplication.java
+++ springcloud-sample-biz/springcloud-sample-hello/src/main/java/com/year/HelloApplication.java
@@ -1,38 +1,36 @@
 package com.year;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableDiscoveryClient
+@EnableAutoConfiguration
 @RestController
-public class OrderApplication {
+public class HelloApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderApplication.class,args);
+        SpringApplication.run(HelloApplication.class, args);
     }
 
-    @Autowired
+        /*@Autowired
     DiscoveryClient discoveryClient;
 
     @RequestMapping("/services")
     public List<String> services(){
         return discoveryClient.getServices();
-    }
+    }*/
 
-    @Value("${key}")
-    private String key;
+    @Value("${hello}")
+    private String hello;
 
-    @RequestMapping("/getKey")
-    public String getKey(){
-        return key;
+    @RequestMapping("hello")
+    public String hello() {
+        return hello;
     }
 }
